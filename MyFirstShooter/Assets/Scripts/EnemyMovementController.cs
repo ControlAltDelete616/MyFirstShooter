18264988a6902cae486c67919efd5f0b8c0b66bd
ControlAltDelete616 <fabrizio.florio616@gmail.com>
2020-02-11 13:36:52 +0100

    Enemy & EnemyAnimationController Script implemented

---
+++ MyFirstShooter/Assets/Scripts/EnemyMovementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementController : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = player.transform.position;
+    }
+}
